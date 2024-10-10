b7664c4a59dd4cb91bfc47a82766587beb5f0371
holgerpart <103240549+holgerpart@users.noreply.github.com>
2024-10-10 11:19:47 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,14 +1,15 @@
-function skillsmember () {
-    return {
-        restrict: 'E',
-        scope: {
-            skills: '='
-        },
-        template: '<div class="skills">' +
-            '<div ng-repeat="skill in skills" class="skill">' +
-            '<span class="name">{{skill.name}}</span>' +
-            '<div class="level" ng-style="{width: skill.level + \'%\'}"></div>' +
-            '</div>' +
-            '</div>'
-    };
+function skillsmember() {
+  var member = document.getElementById("member").value;
+  var skills = document.getElementById("skills").value;
+  var data = { member: member, skills: skills };
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/skillsmember", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(data));
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log("skillsmember");
+      console.log(xhr.responseText);
+    }
+  };
 }
