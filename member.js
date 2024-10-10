3bba63ea347230a373164a2227e39bdb7cd07617
holgerpart <103240549+holgerpart@users.noreply.github.com>
2024-10-10 11:11:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsmember() {
+    return {
+        restrict: 'E',
+        scope: {
+            skills: '='
+        },
+        template: '<div class="skills">' +
+            '<div ng-repeat="skill in skills" class="skill">' +
+            '<span class="name">{{skill.name}}</span>' +
+            '<div class="level" ng-style="{width: skill.level + \'%\'}"></div>' +
+            '</div>' +
+            '</div>'
+    };
+}
